a504b2070934857f269b57ed505b2bd44b23099c
sachin <2107sachin@gmail.com>
2021-02-20 09:11:36 +0530

    spring-cloud-api-gateway first commit

---
+++ spring-cloud-api-gateway/src/main/java/com/microservices/springcloudapigateway/SpringCloudApiGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.microservices.springcloudapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+public class SpringCloudApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudApiGatewayApplication.class, args);
+	}
+
+}
